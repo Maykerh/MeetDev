8aa2bf0670f1eadc63921e2c70fafa1294079f8a
Mayke <make_zs@hotmail.com>
2019-08-29 23:45:54 -0300

    implementado upload de imagens

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,20 @@
 import { Router } from 'express';
-
+import multer from 'multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import multerConfig from './config/multer';
 import AuthMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(AuthMiddleware);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 routes.put('/users/:id', UserController.update);
 
